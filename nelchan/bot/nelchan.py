99ff1c1f8210b2da582135f3b818962613e979ea
sh1ma <in9lude@gmail.com>
2021-06-05 16:55:04 +0900

    chore: ディレクトリ名変えた

--- nelchan/bot/nelchan.py
+++ nelchan/bot/nelchan.py
@@ -8,4 +8,4 @@ class NelChan(Bot):
         super().__init__(command_prefix=commands_prefix)
 
         for cog in COGS:
-            self.load_extension("nelchan.app.cogs." + cog)
+            self.load_extension("nelchan.bot.cogs." + cog)
