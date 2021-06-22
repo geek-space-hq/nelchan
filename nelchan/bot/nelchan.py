59e2da2c7c2aa010a01d16ace31c646112036e82
sh1ma <in9lude@gmail.com>
2021-06-22 17:44:12 +0900

    これでどうすか

--- nelchan/bot/nelchan.py
+++ nelchan/bot/nelchan.py
@@ -5,7 +5,7 @@ COGS = ["ready", "topic_ch", "dictionary"]
 
 class NelChan(Bot):
     def __init__(self, commands_prefix: tuple[str, ...] = ("nel,", "Nel, ")):
-        super().__init__(command_prefix=when_mentioned_or(commands_prefix))
+        super().__init__(command_prefix=when_mentioned_or(*commands_prefix))
 
         for cog in COGS:
             self.load_extension("nelchan.bot.cogs." + cog)
